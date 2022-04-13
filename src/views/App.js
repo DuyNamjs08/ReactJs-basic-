f2c61f71edab051ffc8499ca7d366b3cefa8efdc
duynam08 <duynam11a11999@gmail.com>
2022-04-13 20:54:37 +0700

    second update

--- src/views/App.js
+++ src/views/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>xin chào Nam đz</p>
         <Component />
       </header>
     </div>
--- src/views/example/Component.js
+++ src/views/example/Component.js
@@ -1,21 +1,33 @@
 import react from "react";
 
-class Component extends react.Component{
-    state={
-        arrJobs:[{ id:'job1' , title:'Javascript', salary: '500'},
-       { id:'job2' , title:'Php', salary: '1500'},
-        { id:'job2' , title:'Ruby', salary: '700'}
-        ]
-    }
+class Component extends react.Component {
+  state = {
+    name: "",
+    chanel: "Namduy08",
+  };
+  handleOnChange=(event)=>{
+      console.log('event :',event ,'event.target : ' , event.target , 'event.target.value :', event.target.value);
+      this.setState({
+          name : event.target.value
+      })
+  }
 
-
-    render(){
-        let {arrJobs}=this.state
-        return (
-          <>
-            <div className="State-container"> {this.state.arrJobs}</div>
-          </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="first">
+          <input
+            type='text'
+            value={this.state.name}
+            onChange={(event) => this.handleOnChange(event)}
+          /><br/>
+          chào các bạn mình tên là : {this.state.name}
+        </div>
+        <div className="second">
+          kênh youtube của mình là : {this.state.chanel}
+        </div>
+      </>
+    );
+  }
 }
 export default Component
