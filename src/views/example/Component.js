7bc12cc66055d0d5b123cf675ff78a8a9fc49511
duynam08 <duynam11a11999@gmail.com>
2022-04-13 21:08:26 +0700

    third update

--- src/views/example/Component.js
+++ src/views/example/Component.js
@@ -5,11 +5,21 @@ class Component extends react.Component {
     name: "",
     chanel: "Namduy08",
   };
-  handleOnChange=(event)=>{
-      console.log('event :',event ,'event.target : ' , event.target , 'event.target.value :', event.target.value);
-      this.setState({
-          name : event.target.value
-      })
+  handleOnChange = (event) => {
+    console.log(
+      "event :",
+      event,
+      "event.target : ",
+      event.target,
+      "event.target.value :",
+      event.target.value
+    );
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnclick=()=>{
+      alert('bạn đã click')
   }
 
   render() {
@@ -17,14 +27,18 @@ class Component extends react.Component {
       <>
         <div className="first">
           <input
-            type='text'
+            type="text"
             value={this.state.name}
             onChange={(event) => this.handleOnChange(event)}
-          /><br/>
+          />
+          <br />
           chào các bạn mình tên là : {this.state.name}
         </div>
         <div className="second">
           kênh youtube của mình là : {this.state.chanel}
+        </div>
+        <div>
+          <button onClick={(event) => this.handleOnclick(event)}>click me !</button>
         </div>
       </>
     );
