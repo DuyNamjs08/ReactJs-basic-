628b05829930cf992762fb26cf51ea1256f99382
duynam08 <duynam11a11999@gmail.com>
2022-04-13 21:48:56 +0700

    four update

--- src/views/example/Component.js
+++ src/views/example/Component.js
@@ -2,43 +2,50 @@ import react from "react";
 
 class Component extends react.Component {
   state = {
-    name: "",
-    chanel: "Namduy08",
+    firstName: "",
+    lastName: "",
   };
-  handleOnChange = (event) => {
-    console.log(
-      "event :",
-      event,
-      "event.target : ",
-      event.target,
-      "event.target.value :",
-      event.target.value
-    );
+  handleOnChangleFirstname = (event) => {
     this.setState({
-      name: event.target.value,
+      firstName: event.target.value,
     });
   };
-  handleOnclick=()=>{
-      alert('bạn đã click')
-  }
+  handleOnChangleLastname = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+  handleOnclick = () => {
+    alert("hello");
+    console.log(this.state);
+  };
 
   render() {
     return (
       <>
-        <div className="first">
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={(event) => this.handleOnChange(event)}
-          />
-          <br />
-          chào các bạn mình tên là : {this.state.name}
-        </div>
-        <div className="second">
-          kênh youtube của mình là : {this.state.chanel}
-        </div>
         <div>
-          <button onClick={(event) => this.handleOnclick(event)}>click me !</button>
+          <form>
+            <label htmlFor="fistName">First name:</label>
+            <br />
+            <input
+              type="text"
+              value={this.state.firstName}
+              onChange={(event) => this.handleOnChangleFirstname(event)}
+            />
+            <br />
+            <label htmlFor="lastName">Last name:</label>
+            <br />
+            <input
+              type="text"
+              value={this.state.lastName}
+              onChange={(event) => this.handleOnChangleLastname(event)}
+            />
+          </form>
+          <div>
+            <button type="text" onClick={() => this.handleOnclick()}>
+              click me !
+            </button>
+          </div>
         </div>
       </>
     );
